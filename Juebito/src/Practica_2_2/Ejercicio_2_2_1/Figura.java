518d998aed160e6f679b4a9f251cdf419ececc1f
SebaMediza <sebamediza@hotmail.com>
2023-04-03 13:57:56 -0300

    herencia

---
+++ Juebito/src/Practica_2_2/Ejercicio_2_2_1/Figura.java
@@ -0,0 +1,10 @@
+package Practica_2_2.Ejercicio_2_2_1;
+
+public class Figura {
+    protected String identificadorString;
+    
+    public Figura(){
+        this.identificadorString = "Esto es una figura";
+        System.out.println(identificadorString);
+    }
+}
