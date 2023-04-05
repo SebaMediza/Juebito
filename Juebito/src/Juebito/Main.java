48cea2ac452e1ff6f99109e8f046f74ba3cc4371
SebaMediza <sebamediza@hotmail.com>
2023-04-05 17:23:41 -0300

    Arreglo

--- Juebito/src/Juebito/Main.java
+++ Juebito/src/Juebito/Main.java
@@ -7,29 +7,31 @@ import java.util.Scanner;
 public class Main {
    public static void main(String[] args) {
       Vector<Juego> games = new Vector<>();
+      int indice;
       try (Scanner input = new Scanner(System.in)) {
          int eleccion;
          do{
             System.out.println("1: Juego Dado");
             System.out.println("2: Juego Piedra, Pepel o Tijera");
             System.out.println("0: Salir");
+            Dados dice = new Dados();
+            games.add(0, dice);
+            PiedraPapelTijera juegar = new PiedraPapelTijera();
+            games.add(1,juegar);
+
             eleccion = input.nextInt();
             switch(eleccion){
                case 1:
-               Dados dice = new Dados();
-               games.add(dice);
-               games.get(0).jugar();
-               System.out.println("El resultado es: ");
-               System.out.println(games.get(0).mostrarResultado());
-               System.out.println("");
+                  indice = 0;
+                  games.get(indice).jugar();
+                  System.out.println("El resultado es: ");
+                  System.out.println(games.get(0).mostrarResultado());
                break;
                case 2:
-               PiedraPapelTijera juegar = new PiedraPapelTijera();
-               games.add(juegar);
-               games.get(0).jugar();
-               System.out.println("El resultado es: ");
-               System.out.println(games.get(0).mostrarResultado());
-               System.out.println("");
+                  indice = 1;
+                  games.get(indice).jugar();
+                  System.out.println("El resultado es: ");
+                  System.out.println(games.get(0).mostrarResultado());
                break;
             }
          }while(eleccion != 0);
