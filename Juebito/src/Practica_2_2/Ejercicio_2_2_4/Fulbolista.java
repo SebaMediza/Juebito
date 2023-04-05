d3197a63299b911531b752d47463bfb4e84384dc
SebaMediza <sebamediza@hotmail.com>
2023-04-05 11:21:32 -0300

    2.2.4

---
+++ Juebito/src/Practica_2_2/Ejercicio_2_2_4/Fulbolista.java
@@ -0,0 +1,5 @@
+package Practica_2_2.Ejercicio_2_2_4;
+
+public class Fulbolista extends SeleccionFutbol {
+    
+}
---
+++ Juebito/src/Practica_2_2/Ejercicio_2_2_4/IntegranteSeleccionFutbol.java
@@ -0,0 +1,8 @@
+package Practica_2_2.Ejercicio_2_2_4;
+
+public interface IntegranteSeleccionFutbol {
+    static void concentrarce(){}
+    static void viajar(){}
+    static void entrenar(){}
+    static void jugarPartido(){}
+}
