f081b1e25eba3bc3d12210e3b20f4ce29b14ef4f
SebaMediza <sebamediza@hotmail.com>
2023-04-05 15:30:58 -0300

    Arreglitos

--- Juebito/src/Practica_2_1/Ejercicio_2_1_4/Carta.java
+++ Juebito/src/Practica_2_1/Ejercicio_2_1_4/Carta.java
@@ -1,10 +1,10 @@
 package Practica_2_1.Ejercicio_2_1_4;
 
 public class Carta {
-    private int pica;
-    private int corazon;
-    private int diamante;
-    private int trebol;
+    // private int pica;
+    // private int corazon;
+    // private int diamante;
+    // private int trebol;
 
     public void generarCarta(){}
 
