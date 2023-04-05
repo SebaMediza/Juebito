87f703cabc1f945fd77b1174382a4d60061dc83b
SebaMediza <sebamediza@hotmail.com>
2023-04-05 14:17:44 -0300

    Creacion de clases

--- Juebito/src/Juebito/Juego.java
+++ Juebito/src/Juebito/Juego.java
@@ -1,6 +1,11 @@
 package Juebito;
 
 public abstract class Juego {
-    private String reusltado;
+    private String resultado;
+    
     public abstract void jugar();
+    
+    public String mostrarResultado() {
+        return this.resultado;
+    }
 }
