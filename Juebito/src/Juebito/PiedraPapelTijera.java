c02c0ce76d586a07a0569b2b949be138270e43c8
david99pereyra <david99pereyra@gmail.com>
2023-04-05 15:28:35 -0300

    set and get PiedraPapelTijera

--- Juebito/src/Juebito/PiedraPapelTijera.java
+++ Juebito/src/Juebito/PiedraPapelTijera.java
@@ -1,7 +1,39 @@
 package Juebito;
 
 public class PiedraPapelTijera extends Juego{
+    private String pierda = "piedra";
+    private String papel = "papel";
+    private String tijera = "tijera";
+    
+
+
+    public String getPierda() {
+        return this.pierda;
+    }
+
+    public void setPierda(String pierda) {
+        this.pierda = pierda;
+    }
+
+    public String getPapel() {
+        return this.papel;
+    }
+
+    public void setPapel(String papel) {
+        this.papel = papel;
+    }
+
+    public String getTijera() {
+        return this.tijera;
+    }
+
+    public void setTijera(String tijera) {
+        this.tijera = tijera;
+    }
+
+
     public void jugar(){
+
         
     }
     
