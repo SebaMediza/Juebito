5e83e80287ea9909012eaa792041f1edc452433e
SebaMediza <sebamediza@hotmail.com>
2023-04-10 15:16:44 -0300

    Arranque

--- Juebito/src/Practica_2_3/Ejercicio_2_3_2/Calculadora.java
+++ Juebito/src/Practica_2_3/Ejercicio_2_3_2/Calculadora.java
@@ -1,36 +1,35 @@
-package Practica_3.Ejercicio_2_3_1;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+package Practica_2_3.Ejercicio_2_3_2;
+import java.awt.event.*;
 import java.awt.*;
-public class Calculadora extends Frame implements ActionListener {
+import javax.swing.*;
 
-    Button b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17, b18;
-    TextField result;
+public class Calculadora extends Panel implements ActionListener {
 
-    public button() {
-        b0 = new Button("0");
-        b1 = new Button("1");
-        b2 = new Button("2");
-        b3 = new Button("3");
-        b4 = new Button("4");
-        b5 = new Button("5");
-        b6 = new Button("6");
-        b7 = new Button("7");
-        b8 = new Button("8");
-        b9 = new Button("9");
-        b10 = new Button("/");
-        b11 = new Button("*");
-        b12 = new Button("-");
-        b13 = new Button("+");
-        b14 = new Button(",");
-        b15 = new Button("CE");
-        b16 = new Button("C");
-        b17 = new Button("=");
-        b18 = new Button("<-");
+    JButton b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17, b18;
+    JTextField result;
 
-        result = new TextField();
+    public Calculadora() {
+        b0 = new JButton("0");
+        b1 = new JButton("1");
+        b2 = new JButton("2");
+        b3 = new JButton("3");
+        b4 = new JButton("4");
+        b5 = new JButton("5");
+        b6 = new JButton("6");
+        b7 = new JButton("7");
+        b8 = new JButton("8");
+        b9 = new JButton("9");
+        b10 = new JButton("/");
+        b11 = new JButton("*");
+        b12 = new JButton("-");
+        b13 = new JButton("+");
+        b14 = new JButton(",");
+        b15 = new JButton("CE");
+        b16 = new JButton("C");
+        b17 = new JButton("=");
+        b18 = new JButton("<-");
+
+        result = new JTextField();
 
         this.add(b0);
         this.add(b1);
@@ -51,10 +50,37 @@ public class Calculadora extends Frame implements ActionListener {
         this.add(b16);
         this.add(b17);
         this.add(b18);
+        this.add(result);
+
 
     }
 
+    public void mostrar(){
+        Frame f = new Frame("Botones Nuevo");
+        GridBagConstraints c = new GridBagConstraints();
+        setFont(new Font("SansSerif", Font.PLAIN, 14));
+        f.setLayout(new GridBagLayout());
+
+        WindowListener l = new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        };
+        f.addWindowListener(l);
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 3.0;
+        f.add(this.result, c);
+        c.gridwidth = GridBagConstraints.REMAINDER; //end row
+        f.add(this.b18, c);
+        f.pack();
+        f.setVisible(true);
+    }
+
+
     public static void main(String[] args) {
+        Calculadora cal = new Calculadora();
+        cal.mostrar();
+
     }
 
     public void actionPerformed(ActionEvent evt) {
